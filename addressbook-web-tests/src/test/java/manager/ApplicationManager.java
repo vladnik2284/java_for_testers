f08ba793c291f626c1bb4aa4b3c7b1c8a038edf8
Vladimir Nikolaev <vladnik2284@yandex.ru>
2026-01-03 20:39:19 +0400

    Добавил параметр запуска браузера

--- addressbook-web-tests/src/test/java/manager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/manager/ApplicationManager.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
@@ -13,9 +14,15 @@ public class ApplicationManager {
     private GroupHelper groups;
 
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new FirefoxDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown drowser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/");
             driver.manage().window().setSize(new Dimension(1382, 693));
--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -10,7 +10,8 @@ public class TestBase {
     public void setUp() {
         if (app == null) {
             app = new ApplicationManager();
+            app.init(System.getProperty("browser", "firefox"));
         }
-        app.init();
+
     }
 }
