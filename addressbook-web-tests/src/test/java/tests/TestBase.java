293b9c6323c108041db57913d3a2e95a526b31dd
Vladimir Nikolaev <vladnik2284@yandex.ru>
2026-01-10 23:09:47 +0400

    Добавил провайдер для создания групп

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -16,12 +16,12 @@ public class TestBase {
         }
     }
 
-    public String randomString(int n) {
+    public static String randomString(int n) {
         var rnd = new Random();
         var result = "";
         for (int i = 0; i < n; i++) {
             result = result + (char)('a' + rnd.nextInt(26));
         }
-        return result;
+         return result;
     }
 }
